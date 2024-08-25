44854d073dca75e6901b5824d8a3f7e57f86f8ed
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2024-08-25 20:03:04 +0530

    Add files via upload

---
+++ Demo Program/Add_num.java
@@ -0,0 +1,9 @@
+//java Class 23rd Aug
+class Add_num
+{
+	public static void main(String args[])
+	{
+		int a=10, b=13, c=a+b;
+		System.out.println("A + B = " +c);
+	}
+}
---
+++ Demo Program/Myname.java
@@ -0,0 +1,11 @@
+//To Print a Name
+class Myname
+{
+	public static void main(String args[])
+	{	
+		String name="Mohan";
+		String Branch="CSE_AI";
+		String Year="II-Year";
+		System.out.println("I'm" + name + "And Iam Purching" + Branch + Year);
+	}
+}
