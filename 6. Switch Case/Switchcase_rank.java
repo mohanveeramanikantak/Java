2a4c7e4134b18074e5bd8379d571a0cc7cd60fc7
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2024-09-16 20:14:26 +0530

    Add files via upload

---
+++ 6. Switch Case/Switchcase_rank.java
@@ -0,0 +1,31 @@
+// to find rank by using switch case
+class Switchcase_rank
+{
+	public static void main(String args[])
+	{
+		int rank=3;
+		System.out.println("1=first");
+		System.out.println("2=second");
+		System.out.println("3=third");
+		System.out.println("enter the rank");
+		switch(rank)
+		{
+	                case 1:
+				System.out.println("first");
+				break;
+			case 2:
+				System.out.println("second");
+				break;
+			case 3:
+				System.out.println("third");		
+				break;
+			default:
+				System.out.println("only have 3 places");
+				break;
+		}
+	}
+}
+				
+				
+			
+				
