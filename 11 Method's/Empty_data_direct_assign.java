625c0100b400363db038e95db706cdd893a6ed9b
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2024-10-18 20:41:32 +0530

    Add files via upload

---
+++ 11 Method's/Empty_data_direct_assign.java
@@ -0,0 +1,14 @@
+// to print Java Methos  empty in  data direct assing
+class Empty_data_direct_assign
+{
+	void myname()
+	{
+		String Myname="Manikanta";
+		System.out.println("Myname is : " +Myname);
+	}
+	public static void main(String args[])
+	{
+		Empty_data_direct_assign ed=new Empty_data_direct_assign();
+		ed.myname();
+	}
+}
