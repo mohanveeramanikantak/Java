720962f4ff9fa217b3b8390239bda0044897aa03
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2024-10-20 21:39:50 +0530

    Add files via upload

---
+++ 11 Method's/Single_parameter_Statement.java
@@ -0,0 +1,13 @@
+// to print java methods in single parameter in statement model
+class Single_parameter_Statement
+{
+	void laptop(int a)
+	{
+		System.out.println( "a value is : 23");
+	}
+	public static void main(String args[])
+	{
+		Single_parameter_Statement sps=new Single_parameter_Statement();
+		sps.laptop(23);
+	}
+}	
