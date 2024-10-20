d1b47d5b4dd3d8dd915b76ba6f3129128c999597
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2024-10-20 21:42:17 +0530

    Add files via upload

---
+++ 11 Method's/Single_parameter_data_direct_assing.java
@@ -0,0 +1,14 @@
+// to print java methods in single parameter with data direct assign
+class Single_parameter_data_direct_assing
+{
+	void phone (int value)
+	{
+		int Value = 10000;
+		System.out.println(" value is : " +value);
+	}
+	public static void main(String args[])
+	{
+		Single_parameter_data_direct_assing spd=new Single_parameter_data_direct_assing();
+		spd.phone(10000);
+	}
+}
