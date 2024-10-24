7e451dddf7e9b613ba67971881ef677e541ea150
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2024-10-24 21:54:02 +0530

    Add files via upload

---
+++ 12 Class_Methods/Empty_statement.java
@@ -0,0 +1,13 @@
+// wellcome to java methods empty in statement model
+class Empty_statement
+{
+	void wel_msg()
+	{
+		System.out.println(" Welcome to java methods ");
+	}
+	public static void main(String args[])
+	{
+		Empty_statement es=new Empty_statement();
+		es.wel_msg();
+	}
+}
