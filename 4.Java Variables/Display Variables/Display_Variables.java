b8736f451005370f015bb9cd4bc997f47ab0ea2c
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2025-03-01 21:06:38 +0530

    Add files via upload

---
+++ 4.Java Variables/Display Variables/Display_Variables.java
@@ -0,0 +1,10 @@
+class Display_Variables
+{
+	public static void main(String[] args)
+	{
+		String name="Mohan";
+		System.out.println(name);
+	}
+}
+//String name = "Mohan";
+//System.out.println(name);
