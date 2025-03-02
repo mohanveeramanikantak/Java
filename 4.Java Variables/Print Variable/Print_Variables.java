c15666df64352bc783a1b118337da1dad4f6ecc5
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2025-03-02 15:22:20 +0530

    Add files via upload

---
+++ 4.Java Variables/Print Variable/Print_Variables.java
@@ -0,0 +1,10 @@
+class Print_Variables
+{
+	public static void main(String[] args)
+	{
+		String firstName = "Moh";
+		String lastName = "an";
+		String fullName = firstName + lastName;
+		System.out.println(fullName);
+	}	
+}
