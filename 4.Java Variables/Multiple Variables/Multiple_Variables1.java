9ad314929877b461b9ec955c34225bdec448cffd
Kalepu Mohan Veera Manikanta <145757595+mohanveeramanikantak@users.noreply.github.com>
2025-03-03 18:59:13 +0530

    Add files via upload

---
+++ 4.Java Variables/Multiple Variables/Multiple_Variables1.java
@@ -0,0 +1,13 @@
+//Declare Many Variables
+class Multiple_Variables1
+{
+	public static void main(String[] args)
+	//public static void main(String[] args)
+	{
+		int x=10;
+		int y=20;
+		int z=30;
+		System.out.println(x+y+z);
+	}	
+}
+	
